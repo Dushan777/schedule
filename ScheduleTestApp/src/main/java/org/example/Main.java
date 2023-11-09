8fce5d329c938bd5a161e7cfb1090e437183c2ec
Dušan Čolić <dcolic3121rn@raf.rs>
2023-11-09 18:28:22 +0100

    dodat filter by anything i by time or additional data

--- ScheduleTestApp/src/main/java/org/example/Main.java
+++ ScheduleTestApp/src/main/java/org/example/Main.java
@@ -46,8 +46,8 @@ public class Main {
             System.out.println("6. All free terms");//
             System.out.println("7. All booked terms");//
             System.out.println("8. Filter by room"); // true,RAF1,30,Racunar,2,Projektor,1
-            System.out.println("9. Filter by time");
-            System.out.println("10. Filter by anything");
+            System.out.println("9. Filter by time or additional data");//false,01/01/2024,01/13/2024,09:15,11:15,null,Wednesday
+            System.out.println("10. Filter by anything");//
             System.out.println("11. Load from CSV");// termini1,config1
             System.out.println("12. Load from JSON");
             System.out.println("13. Save to CSV");//   exportCSV
@@ -212,7 +212,7 @@ public class Main {
                             termini = schedule.filterByRooms(splitLine[1],Integer.parseInt(splitLine[2]),h2,Boolean.parseBoolean(splitLine[0]));
                             termini.forEach(System.out::println);
                         }
-                        else if(size == 4)
+                        else if(size == 4 && splitLine[3] == null)
                         {
                             termini = schedule.filterByRooms(splitLine[1],Integer.parseInt(splitLine[2]),null,Boolean.parseBoolean(splitLine[0]));
                             termini.forEach(System.out::println);
@@ -227,9 +227,172 @@ public class Main {
                     break;
                 case 9:
                     System.out.println("Filter by time or additional data");
+                    scanner = new Scanner(System.in);
+                    line1 = scanner.nextLine();
+                    splitLine = line1.split(",");
+                    size = splitLine.length;
+                    for(int i = 0 ; i < splitLine.length; i++)
+                    {
+                        if(splitLine[i].equals("null"))
+                            splitLine[i] = null;
+
+                    }
+                    if(splitLine[1] == null) {
+                        String m,d, y;
+                        m = String.valueOf(schedule.getBeginningDate().getMonth().getValue());
+                        d = String.valueOf(schedule.getBeginningDate().getDayOfMonth());
+                        y = String.valueOf(schedule.getBeginningDate().getYear());
+                        if(Integer.parseInt(d) < 10)
+                            d = "0"+d;
+                        if(Integer.parseInt(m) < 10)
+                            m = "0"+m;
+                        splitLine[1] = m+"/"+d+"/"+y;
+                    }
+                    if(splitLine[2] == null) {
+                        String m,d, y;
+                        m = String.valueOf(schedule.getEndingDate().getMonth().getValue());
+                        d = String.valueOf(schedule.getEndingDate().getDayOfMonth());
+                        y = String.valueOf(schedule.getEndingDate().getYear());
+                        if(Integer.parseInt(d) < 10)
+                            d = "0"+d;
+                        if(Integer.parseInt(m) < 10)
+                            m = "0"+m;
+                        splitLine[2] = m+"/"+d+"/"+y;
+                    }
+                    if(splitLine[3] == null)
+                        splitLine[3] = "00:00";
+                    if(splitLine[4] == null)
+                        splitLine[4] = "23:59";
+                    Map<String, String> h3 = new HashMap<>();
+                    try {
+                        if(size >= 8 && size%2==0)
+                        {
+                            for(int i = 5; i < size; i+=2)
+                            {
+                                h3.put(splitLine[i],splitLine[i+1]);
+                            }
+                           termini = schedule.filterByTimeOrAdditionalData(new Time(LocalDate.parse(splitLine[1],formatter),LocalDate.parse(splitLine[2],formatter),LocalTime.parse(splitLine[3],formatter1),LocalTime.parse(splitLine[4],formatter1)),h3,splitLine[6],Boolean.parseBoolean(splitLine[0]));
+                            termini.forEach(System.out::println);
+                        }
+                        else if(size == 7 && splitLine[5] == null)
+                        {
+                            termini = schedule.filterByTimeOrAdditionalData(new Time(LocalDate.parse(splitLine[1],formatter),LocalDate.parse(splitLine[2],formatter),LocalTime.parse(splitLine[3],formatter1),LocalTime.parse(splitLine[4],formatter1)),null,splitLine[6],Boolean.parseBoolean(splitLine[0]));
+                            termini.forEach(System.out::println);
+                        }
+                        else
+                        {
+                            System.out.println("Invalid input");
+                        }
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case 10:
                     System.out.println("Filter by anything");
+                    scanner = new Scanner(System.in);
+                    line1 = scanner.nextLine();
+                    if(!line1.contains(" "))
+                    {
+                        System.out.println("Invalid input");
+                        break;
+                    }
+                    splitLine = line1.split(" ");
+                    String splitL = splitLine[0];
+                    String splitL2 = splitLine[1];
+                    String[] splitLine2 = splitL.split(",");
+                    String[] splitLine3 = splitL2.split(",");
+
+                    size = splitLine2.length;
+                    int size2 = splitLine3.length;
+
+                    for(int i = 0 ; i < splitLine2.length; i++)
+                    {
+                        if(splitLine2[i].equals("null"))
+                            splitLine2[i] = null;
+
+                    }
+                    for(int i = 0 ; i < splitLine3.length; i++)
+                    {
+                        if(splitLine3[i].equals("null"))
+                            splitLine3[i] = null;
+
+                    }
+
+                    if(splitLine3[0] == null) {
+                        String m,d, y;
+                        m = String.valueOf(schedule.getBeginningDate().getMonth().getValue());
+                        d = String.valueOf(schedule.getBeginningDate().getDayOfMonth());
+                        y = String.valueOf(schedule.getBeginningDate().getYear());
+                        if(Integer.parseInt(d) < 10)
+                            d = "0"+d;
+                        if(Integer.parseInt(m) < 10)
+                            m = "0"+m;
+                        splitLine3[0] = m+"/"+d+"/"+y;
+                    }
+                    if(splitLine3[1] == null) {
+                        String m,d, y;
+                        m = String.valueOf(schedule.getEndingDate().getMonth().getValue());
+                        d = String.valueOf(schedule.getEndingDate().getDayOfMonth());
+                        y = String.valueOf(schedule.getEndingDate().getYear());
+                        if(Integer.parseInt(d) < 10)
+                            d = "0"+d;
+                        if(Integer.parseInt(m) < 10)
+                            m = "0"+m;
+                        splitLine3[1] = m+"/"+d+"/"+y;
+                    }
+                    if(splitLine3[2] == null)
+                        splitLine3[2] = "00:00";
+                    if(splitLine3[3] == null)
+                        splitLine3[3] = "23:59";
+
+                    Map<String, Integer> h4 = new HashMap<>();
+                    Map<String, String> h5 = new HashMap<>();
+
+                    try {
+                        if(size >= 5 && size%2==1)
+                        {
+                            for(int i = 3; i < size; i+=2)
+                            {
+                                h4.put(splitLine2[i], Integer.valueOf(splitLine2[i+1]));
+                            }
+
+                        }
+                        if(size2 >= 7 && size2%2==1)
+                        {
+                            for(int i = 4; i < size2; i+=2)
+                            {
+                                h5.put(splitLine3[i], splitLine3[i+1]);
+                            }
+
+                        }
+                        if(size2 == 6 && splitLine3[4] == null && size == 4 && splitLine2[3] == null)
+                        {
+                            termini = schedule.filterByEverything(splitLine2[1], Integer.parseInt(splitLine2[2]),null,new Time(LocalDate.parse(splitLine3[0],formatter),LocalDate.parse(splitLine3[1],formatter),LocalTime.parse(splitLine3[2],formatter1),LocalTime.parse(splitLine3[3],formatter1)),null,splitLine3[5],Boolean.parseBoolean(splitLine2[0]));
+                            termini.forEach(System.out::println);
+                        }
+                        else if(size2 >= 7 && size2%2==1 && size == 4 && splitLine2[3] == null)
+                        {
+                            termini = schedule.filterByEverything(splitLine2[1], Integer.parseInt(splitLine2[2]),null,new Time(LocalDate.parse(splitLine3[0],formatter),LocalDate.parse(splitLine3[1],formatter),LocalTime.parse(splitLine3[2],formatter1),LocalTime.parse(splitLine3[3],formatter1)),h5,splitLine3[5],Boolean.parseBoolean(splitLine2[0]));
+                            termini.forEach(System.out::println);
+                        }
+                        else if(size2 == 6 && splitLine3[4] == null && size >= 5 && size%2==1)
+                        {
+                            termini = schedule.filterByEverything(splitLine2[1], Integer.parseInt(splitLine2[2]),h4,new Time(LocalDate.parse(splitLine3[0],formatter),LocalDate.parse(splitLine3[1],formatter),LocalTime.parse(splitLine3[2],formatter1),LocalTime.parse(splitLine3[3],formatter1)),null,splitLine3[5],Boolean.parseBoolean(splitLine2[0]));
+                            termini.forEach(System.out::println);
+                        }
+                        else if(size2 >= 7 && size2%2==1 && size >= 5 && size%2==1)
+                        {
+                            termini = schedule.filterByEverything(splitLine2[1], Integer.parseInt(splitLine2[2]),h4,new Time(LocalDate.parse(splitLine3[0],formatter),LocalDate.parse(splitLine3[1],formatter),LocalTime.parse(splitLine3[2],formatter1),LocalTime.parse(splitLine3[3],formatter1)),h5,splitLine3[5],Boolean.parseBoolean(splitLine2[0]));
+                            termini.forEach(System.out::println);
+                        }
+
+                        else
+                        {
+                            System.out.println("Invalid input");
+                        }
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case 11:
                     try {
